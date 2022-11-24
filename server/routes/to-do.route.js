ad9b765c5780fd9454cb2365d3a3e14d893966d9
George Reeve <george.fr.reeve@gmail.com>
2022-11-24 15:43:04 +0000

    Add input validation.

--- server/routes/to-do.route.js
+++ server/routes/to-do.route.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const List = require('../../models/toDoModel')
 const newID = require("../utility/snowflake");
+const {body, param} = require("express-validator");
 const toDoRouter = Router()
 
 //get - gets all notes, sends 200 status and all notes an an object - get(localhost:[port]/)
@@ -11,7 +12,11 @@ toDoRouter.get("/", async (req, res) => {
 })
 
 //add/post - posts a note using data from body, sends 201 status - post(localhost:[port]/)
-toDoRouter.post('/', async (req,res) => {
+toDoRouter.post('/',
+  body("title").isString().isAlphanumeric(),
+  body("description").isString().isAlphanumeric(),
+  body("status").isNumeric().custom(input => input >= 1 && input <= 3),
+  async (req,res) => {
     try {
         const body = req.body
         await List.create({
@@ -28,7 +33,11 @@ toDoRouter.post('/', async (req,res) => {
 })
 
 //put - gets id of item and writes new text or status, sends 200 - put(localhost:[port]/)
-toDoRouter.put('/', async (req,res) => {
+toDoRouter.put('/',
+  body("title").optional().isString().isAlphanumeric(),
+  body("description").optional().isString().isAlphanumeric(),
+  body("status").optional().isNumeric().custom(input => input >= 1 && input <= 3),
+  async (req,res) => {
     try {
         const body = req.body
         const item = await List.findByPk(body.id)
@@ -50,7 +59,9 @@ toDoRouter.put('/', async (req,res) => {
 })
 
 //delete - deletes a list item by id - delete(localhost:[port]/:id)
-toDoRouter.delete("/:id", async(req, res) => {
+toDoRouter.delete("/:id",
+  param("id").isString().isNumeric(),
+  async(req, res) => {
     try {
         const id = req.params.id
 
