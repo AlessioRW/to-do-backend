7365649c20f5032d55678d45da13c8386ae7e6e2
George Reeve <george.fr.reeve@gmail.com>
2022-11-24 15:32:04 +0000

    Change list IDs.

--- db/seed.js
+++ db/seed.js
@@ -1,31 +1,32 @@
-const List  = require('../models/toDoModel')
-const db = require('./db')
-
+const List  = require('../models/toDoModel');
+const db = require('./db');
+const newID = require("../server/utility/snowflake");
 
 async function seed() {
-
-    await db.sync({
-        force:true
+    await db.sync( {
+        force: true
     })
 
     await List.create({
+        id: newID().toString(),
         title: 'Really cool note',
         description: 'This note exists',
         status: 1
     })
 
     await List.create({
+        id: newID().toString(),
         title: 'Really cool note',
         description: 'This note exists',
         status: 1
     })
 
     await List.create({
+        id: newID().toString(),
         title: 'Really cool note',
         description: 'This note exists',
         status: 1
     })
-
 }
 
 module.exports = seed
