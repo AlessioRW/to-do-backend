37693bd0548044d51b98e041d1484d719e0b32e2
George Reeve <george.fr.reeve@gmail.com>
2022-11-25 12:21:55 +0000

    Add reminders per user.

---
+++ src/utility/token.js
@@ -0,0 +1,14 @@
+const jsonwebtoken = require("jsonwebtoken");
+const fs = require("fs");
+
+const privateKey = fs.readFileSync("./keys/ec-priv-key.pem");
+
+function sign(id) {
+  return jsonwebtoken.sign({ id }, privateKey, { algorithm: "ES256" });
+}
+
+function verify(token) {
+  return jsonwebtoken.verify(token, privateKey, { algorithms: ["ES256"] });
+}
+
+module.exports = { sign, verify };
